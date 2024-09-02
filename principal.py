669bcaf15033c35194a866de6d9773f9ff514cd9
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-09-02 08:56:54 -0500

    Create principal.py

---
+++ principal.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  1 03:14:41 2023
+
+@author: Freddy J. Orozco R.
+@Powered: WinStats.
+"""
+
+import streamlit as st
+#import hydralit_components as hc
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+#from PIL import Image
+from matplotlib.patches import Rectangle
+import math
+
+############################################################################################################################################################################################################################
+st.set_page_config(layout="wide")
+navigation_tree = {
+    "Menu": [
+        st.Page("cont/OptaEventingData.py", title="Eventing Data", icon=":material/download:"),
+        st.Page("cont/OptaJoinData.py", title="Join Data", icon=":material/cell_merge:"),
+        st.Page("cont/OptaExploreData.py", title="Explore Data", icon=":material/search_insights:"),
+        st.Page("cont/OptaExploreTeamData.py", title="Explore Team Data", icon=":material/analytics:"),        
+        st.Page("cont/OptaProMatchData.py", title="Pro Match Data", icon=":material/leaderboard:")]
+}
+nav = st.navigation(navigation_tree, position="sidebar")
+nav.run()
