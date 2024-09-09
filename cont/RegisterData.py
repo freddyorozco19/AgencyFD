86eb149f97e5fbbfe3bb3c8f5e265cbdb273fe4f
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-09-09 00:00:42 -0500

    Update RegisterData.py

--- cont/RegisterData.py
+++ cont/RegisterData.py
@@ -32,6 +32,9 @@ from matplotlib.patches import Rectangle
 import math
 import io
 from st_aggrid import AgGrid, JsCode, GridOptionsBuilder
+import os
+from google.oauth2.service_account import Credentials
+from googleapiclient.discovery import build
 
 st.header("REGISTER DATA")
 
