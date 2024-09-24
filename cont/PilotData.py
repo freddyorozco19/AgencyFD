813891c1e19f7c610d872056a52ed39836fe131c
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-09-24 14:54:54 -0500

    Update PilotData.py

--- cont/PilotData.py
+++ cont/PilotData.py
@@ -1,4 +1,132 @@
 import streamlit as st
+import datetime
+import base64
+import pandas as pd
+from io import BytesIO
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.pyplot as mplt
+import matplotlib.font_manager as font_manager
+import mplsoccer
+from mplsoccer import Pitch, VerticalPitch, FontManager
+import sklearn
+from sklearn.preprocessing import StandardScaler
+from scipy.spatial import ConvexHull
+from matplotlib.colors import LinearSegmentedColormap
+import matplotlib.patheffects as path_effects
+from scipy.ndimage import gaussian_filter
+import seaborn as sns
+from matplotlib import colors as mcolors
+import requests
+from PIL import Image
+from matplotlib.patches import Rectangle
+import math
+import altair as alt
 
+############################################################################################################################################################################################################################
+def split_minute(minute_str):
+    if '+' in minute_str:
+        parts = minute_str.split('+')
+        return parts[0], parts[1]
+    else:
+        return minute_str, ''
 
+def calcular_radio_desde_gol(x, y, y_gol):
+    radio = math.sqrt(x**2 + (y - y_gol)**2)
+    return radio
+def es_deep_completion(row):
+    radio = math.sqrt(row['X1']**2 + (row['Y1'] - y_gol_oponente)**2)
+    return radio <= radio_umbral
+def esta_dentro_semicircunferencia(x, y):
+    distancia_centro = math.sqrt((x - punto_medio_cancha_rival[0])**2 + (y - punto_medio_cancha_rival[1])**2)
+    return distancia_centro <= radio_umbral
+
+# Función para calcular la distancia euclidiana entre dos puntos
+def distancia_entre_puntos(x1, y1, x2, y2):
+    return np.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+
+#def to_excel(df):
+    #output = BytesIO()
+    #writer = pd.ExcelWriter(output, engine='xlsxwriter')
+    #df.to_excel(writer, index=False, sheet_name='Sheet1')
+    #workbook = writer.book
+    #worksheet = writer.sheets['Sheet1']
+    #format1 = workbook.add_format({'num_format': '0.00'}) 
+    #worksheet.set_column('A:A', None, format1)  
+    #writer.save()
+    #processed_data = output.getvalue()
+    #return processed_data
+def to_excelA(df):
+    output = io.BytesIO()
+    with pd.ExcelWriter(output, engine='xlsxwriter') as writer:
+        df.to_excel(writer, index=False, sheet_name='Sheet1')
+    processed_data = output.getvalue()
+    return processed_data
+
+def convert_df(df):
+    # IMPORTANT: Cache the conversion to prevent computation on every rerun
+    return df.to_csv().encode('utf-8')
+
+def colorlist(color1, color2, num):
+    """Generate list of num colors blending from color1 to color2"""
+    result = [np.array(color1), np.array(color2)]
+    while len(result) < num:
+        temp = [result[0]]
+        for i in range(len(result)-1):
+            temp.append(np.sqrt((result[i]**2+result[i+1]**2)/2))
+            temp.append(result[i+1])
+        result = temp
+    indices = np.linspace(0, len(result)-1, num).round().astype(int)
+    return [result[i] for i in indices] 
+
+hex_list2 = ['#121214', '#D81149', '#FF0050']
+#hex_list = ['#121214', '#112F66', '#004DDD']B91845
+hex_list4 = ['#5A9212', '#70BD0C', '#83E604']
+#hex_list1 = ['#121214', '#854600', '#C36700']
+hex_list = ['#121214', '#545454', '#9F9F9F']
+hex_list1 = ['#121214', '#695E00', '#C7B200']
+#hex_list2 = ['#121214', '#112F66', '#004DDD']
+#hex_list = ['#121214', '#11834C', '#00D570']
+cmap = sns.cubehelix_palette(start=.25, rot=-.3, light=1, reverse=True, as_cmap=True)
+cmap2 = sns.diverging_palette(250, 344, as_cmap=True, center="dark")
+cmap3 = sns.color_palette("dark:#FF0046", as_cmap=True)
+
+def hex_to_rgb(value):
+    value = value.strip("#") # removes hash symbol if present
+    lv = len(value)
+    return tuple(int(value[i:i + lv // 3], 16) for i in range(0, lv, lv // 3))
+
+def rgb_to_dec(value):
+    return [v/256 for v in value]
+
+def get_continuous_cmap(hex_list, float_list=None):
+    rgb_list = [rgb_to_dec(hex_to_rgb(i)) for i in hex_list]
+    if float_list:
+        pass
+    else:
+        float_list = list(np.linspace(0,1,len(rgb_list)))
+    cdict = dict()
+    for num, col in enumerate(['red', 'green', 'blue']):
+        col_list = [[float_list[i], rgb_list[i][num], rgb_list[i][num]] for i in range(len(float_list))]
+        cdict[col] = col_list
+    cmp = mcolors.LinearSegmentedColormap('my_cmp', segmentdata=cdict, N=256)
+    return cmp
+
+#####################################################################################################################################################
+
+font_path = 'Resources/keymer-bold.otf'  # Your font path goes here
+font_manager.fontManager.addfont(font_path)
+prop2 = font_manager.FontProperties(fname=font_path)
+
+font_path2 = 'Resources/BasierCircle-Italic.ttf'  # Your font path goes here
+font_manager.fontManager.addfont(font_path2)
+prop3 = font_manager.FontProperties(fname=font_path2)
+
+###########################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+############################################################################################################################################################################################################################
+
+st.markdown("<style> div { text-align: center } </style>", unsafe_allow_html=True)
 st.header("PILOT DATA")
+
