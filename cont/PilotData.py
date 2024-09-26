08a1dd6f26092f20e9562aa68775c5bad08f669c
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-09-26 12:53:21 -0500

    Update PilotData.py

--- cont/PilotData.py
+++ cont/PilotData.py
@@ -283,10 +283,8 @@ scaled_values = scaler.fit_transform(df[MetricsTTotal])
 dfscaled = pd.DataFrame(scaled_values, columns=MetricsTTotal)
 dfscaledC = pd.concat([df['matchID'], dfscaled], axis=1)
 #st.write(np.mean(new_df00bk['touches_total']))
-st.write(dfscaledC)
+#st.write(dfscaledC)
 #st.write(new_df00bk)
-
-
 ############################################################################################################################################################################################################################
 
 fig, ax = plt.subplots(figsize=(3, 10), dpi = 300)
