d3ccdbfb845c421597259d15d1177bbda7a5caff
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-11-04 05:28:21 -0500

    Update SchedulerData.py

--- cont/SchedulerData.py
+++ cont/SchedulerData.py
@@ -29,3 +29,25 @@ from googleapiclient.discovery import build
 st.markdown("<style> div { text-align: center } </style>", unsafe_allow_html=True)
 st.header("SCHEDULER DATA")
 st.divider()
+
+
+# Define los alcances necesarios para la API de Google Sheets
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
+def read_from_sheets(spreadsheet_id, range_name):
+    """Lee datos de Google Sheets y devuelve un DataFrame usando una cuenta de servicio."""
+    creds = Credentials.from_service_account_file('cont/winstatspilot.json', scopes=SCOPES)
+    service = build('sheets', 'v4', credentials=creds)
+    try:
+        result = service.spreadsheets().values().get(spreadsheetId=spreadsheet_id, range=range_name).execute()
+        values = result.get('values', [])
+        return pd.DataFrame(values[1:], columns=values[0]) if values else None
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+# Ejemplo de uso
+spreadsheet_id = '19KtyXNPL-NoLB6igbG3PJ0grXBS8diEEyjNz27dB0CE'  # Reemplaza con el ID de tu hoja de cálculo
+range_name = 'Hoja2!A1:Z1000'  # Reemplaza con el rango que deseas leer
+
+df = read_from_sheets(spreadsheet_id, range_name)
+st.dataframe(df)
