5b6b39ce3e37fbc89fd74a580a61fc5ceceed23d
Freddy Orozco <60451879+freddyorozco19@users.noreply.github.com>
2024-11-26 00:21:33 -0500

    Update principal.py

--- principal.py
+++ principal.py
@@ -35,16 +35,24 @@ import streamlit_antd_components as sac
 st.set_page_config(layout="wide")
 st.logo("Resources/Img/FootballDivisionWorldwide2.png",icon_image="Resources/Img/FootballDivisionWorldwide2.png")
 
-navigation_tree = {
-    "Menu": [
-        st.Page("cont/MatchAnalysis.py", title="Match Analysis", icon=":material/download:"),
-        st.Page("cont/EventingData.py", title="Eventing Data", icon=":material/analytics:"),
-        st.Page("cont/SchedulerData.py", title="Scheduler Data", icon=":material/event:"),
-        st.Page("cont/RegisterData.py", title="Register Data", icon=":material/leaderboard:")]
-}
-nav = st.navigation(navigation_tree, position="sidebar")
+sac.menu([
+    sac.MenuItem('home', icon='house-fill', tag=[sac.Tag('Tag1', color='green'), sac.Tag('Tag2', 'red')]),
+    sac.MenuItem('products', icon='box-fill', children=[
+        sac.MenuItem('apple', icon='apple'),
+        sac.MenuItem('other', icon='git', description='other items', children=[
+            sac.MenuItem('google', icon='google', description='item description'),
+            sac.MenuItem('gitlab', icon='gitlab'),
+            sac.MenuItem('wechat', icon='wechat'),
+        ]),
+    ]),
+    sac.MenuItem('disabled', disabled=True),
+    sac.MenuItem(type='divider'),
+    sac.MenuItem('link', type='group', children=[
+        sac.MenuItem('antd-menu', icon='heart-fill', href='https://ant.design/components/menu#menu'),
+        sac.MenuItem('bootstrap-icon', icon='bootstrap-fill', href='https://icons.getbootstrap.com/'),
+    ]),
+], open_all=True)
+#nav.run()
 
-nav.run()
-
-st.sidebar.link_button("Source", "https://t.ly/r78Av")
+#st.sidebar.link_button("Source", "https://t.ly/r78Av")
 
